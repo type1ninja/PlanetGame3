5b6e1ce2654b493b39c62fd169736dc69e4ccd35
Type1Ninja <type1ninja@gmail.com>
2017-06-04 10:22:13 -0500

    Tweaked Space Move, Added Textures
    Omnidirectional thrusters (wasd-space-ctrl) are now only usable under a
    specific speed threshold, currently 5.0f. They're also much stronger.
    The main thruster is also weaker than the last commit, but that's
    because the previous value hadn't really been tested enough.
    
    Also, there are now textures/materials for the Earth, the Sun, and a
    ball of metal. So that helps players keep themselves oriented.

--- Assets/Scripts/Player/SpaceMove.cs
+++ Assets/Scripts/Player/SpaceMove.cs
@@ -5,9 +5,11 @@ public class SpaceMove : MonoBehaviour {
     private Rigidbody rigbod;
 
     //WASD + Space + Ctrl thrust force, all directions
-    public float BASE_THRUST = 5.0f;
+    public float BASE_ACCEL = 1.0f;
     //Shift thrust force, forwards
-    public float MAIN_THRUST = 20.0f;
+    public float MAIN_THRUST = 7.5f;
+    //Maximum speed to allow omnidirectional thrust
+    public float MAX_SPEED_FOR_BASE = 5.0f;
 
     private void Start()
     {
@@ -16,14 +18,15 @@ public class SpaceMove : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        //Left/right thrust
-        rigbod.AddForce(transform.right * Input.GetAxis("Horizontal") * BASE_THRUST, ForceMode.Force);
-        //Up/down thrust
-        rigbod.AddForce(transform.up * Input.GetAxis("JumpAxis") * BASE_THRUST, ForceMode.Force);
-        //Only do forward/backward thrust if we're not boosting; don't want to add inputs here
-        if (Input.GetAxis("Thrust") == 0)
+        //Only do omnidirectional thrust if you're going below a certain speed
+        if (rigbod.velocity.magnitude < MAX_SPEED_FOR_BASE)
         {
-            rigbod.AddForce(transform.forward * Input.GetAxis("Vertical") * BASE_THRUST, ForceMode.Force);
+            //Left/right thrust
+            rigbod.AddForce(transform.right * Input.GetAxis("Horizontal") * BASE_ACCEL, ForceMode.VelocityChange);
+            //Up/down thrust
+            rigbod.AddForce(transform.up * Input.GetAxis("JumpAxis") * BASE_ACCEL, ForceMode.VelocityChange);
+            //Forward/backward thrust
+            rigbod.AddForce(transform.forward * Input.GetAxis("Vertical") * BASE_ACCEL, ForceMode.VelocityChange);
         }
 
         rigbod.AddForce(transform.forward * Input.GetAxis("Thrust") * MAIN_THRUST, ForceMode.Force);
