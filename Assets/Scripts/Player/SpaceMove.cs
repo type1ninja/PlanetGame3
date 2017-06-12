f913d618fa1b6a134776383bb5a6ae7e08d3b6d3
Type1Ninja <type1ninja@gmail.com>
2017-06-12 10:16:01 -0500

    Set Up Fuel Sliders
    Set up sliders to indicated how much fuel is left for thrust and how
    much of the cooldown is left on the boost. Also made minor values
    changes to SpaceMove and increased regen speed on the ground. Also
    readded a second planet. I'm probably going to make this a release and
    then begin multiplayer.

--- Assets/Scripts/Player/SpaceMove.cs
+++ Assets/Scripts/Player/SpaceMove.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SpaceMove : MonoBehaviour {
 
     private Rigidbody rigbod;
+    private Slider thrustFuelSlider;
+    private Slider boostCooldownSlider;
 
     //WASD + Space + Ctrl thrust force, all directions
-    public float BASE_ACCEL = 5.0f;
+    public float BASE_ACCEL = 1.0f;
     //Maximum speed to allow omnidirectional thrust
     public float MAX_SPEED_FOR_BASE = 5.0f;
     //Shift thrust force, forwards
     public float MAIN_THRUST = 0.05f;
+    //Shift thrust fuel in seconds
+    public float MAIN_THRUST_FUEL_MAX = 3.0f;
+    //Shift thrust regen cooldown in seconds
+    public float MAIN_THRUST_FUEL_REGEN_MAX = 0.5f;
+    //Shift thrust regen speed
+    public float MAIN_THRUST_FUEL_REGEN_SPEED = 0.5f;
+    //Shift thrust regen speed when grounded
+    public float MAIN_THRUST_FUEL_REGEN_SPEED_GROUNDED = 3.0f;
     //Boost thrust, forwards
     public float BOOST_FORCE = 30.0f;
     //Boost max cooldown in seconds
@@ -17,10 +28,19 @@ public class SpaceMove : MonoBehaviour {
 
     private bool nearSurface = false;
     private float boostCooldown = 0;
+    private float thrustFuel = 1.0f;
+    private float thrustRegenCooldown = 0;
 
     private void Start()
     {
         rigbod = GetComponent<Rigidbody>();
+        thrustFuel = MAIN_THRUST_FUEL_MAX;
+
+        thrustFuelSlider = GameObject.Find("HUDCanvas").transform.Find("ThrustFuelSlider").GetComponent<Slider>();
+        thrustFuelSlider.maxValue = MAIN_THRUST_FUEL_MAX;
+
+        boostCooldownSlider = GameObject.Find("HUDCanvas").transform.Find("BoostCooldownSlider").GetComponent<Slider>();
+        boostCooldownSlider.maxValue = BOOST_COOLDOWN_MAX;
     }
 
     private void FixedUpdate()
@@ -39,13 +59,16 @@ public class SpaceMove : MonoBehaviour {
         //Strong boost is located in Update()
 
         //Main thruster, for orbital adjustments
-        rigbod.AddForce(transform.forward * Input.GetAxis("MainThrust") * MAIN_THRUST, ForceMode.Impulse);
+        if (Input.GetAxis("MainThrust") != 0 && thrustFuel >= 0)
+        {
+            rigbod.AddForce(transform.forward * Input.GetAxis("MainThrust") * MAIN_THRUST, ForceMode.Impulse);
+        } 
     }
 
     private void Update()
     {
-        //Only do strong boost if you're very near a planet and not on cooldown
-        if (nearSurface && boostCooldown <= 0 && Input.GetAxis("Boost") != 0)
+        //Only do strong boost if you're very near a planet, walking, and not on cooldown
+        if (nearSurface && boostCooldown <= 0 && Input.GetAxis("Boost") != 0 && rigbod.velocity.magnitude < MAX_SPEED_FOR_BASE)
         {
             rigbod.AddForce(transform.forward * BOOST_FORCE, ForceMode.Impulse);
             boostCooldown = BOOST_COOLDOWN_MAX;
@@ -56,6 +79,37 @@ public class SpaceMove : MonoBehaviour {
         {
             boostCooldown -= Time.deltaTime;
         }
+
+        //Main thrust fuel consumption and regen
+        if (Input.GetAxis("MainThrust") != 0)
+        {
+            thrustRegenCooldown = MAIN_THRUST_FUEL_REGEN_MAX;
+
+            if (thrustFuel >= 0)
+            {
+                thrustFuel -= Time.fixedDeltaTime;
+            }
+        }
+        if (thrustRegenCooldown <= 0 && thrustFuel < MAIN_THRUST_FUEL_MAX && Input.GetAxis("MainThrust") == 0)
+        {
+            if (nearSurface)
+            {
+                thrustFuel += Time.deltaTime * MAIN_THRUST_FUEL_REGEN_SPEED_GROUNDED;
+            }
+            else
+            { 
+                thrustFuel += Time.deltaTime * MAIN_THRUST_FUEL_REGEN_SPEED;
+            }
+        }
+        else if (thrustRegenCooldown >= 0)
+        {
+            thrustRegenCooldown -= Time.deltaTime;
+        }
+        //Debug.Log("Thrust Fuel: " + thrustFuel + " Thrust Regen Cooldown: " + thrustRegenCooldown);
+
+        //UI Stuff
+        thrustFuelSlider.value = thrustFuel;
+        boostCooldownSlider.value = BOOST_COOLDOWN_MAX - boostCooldown;
     }
 
     private void OnTriggerEnter(Collider other)
