db74692529f88df2abd556c56330b8455d9e69a3
Type1Ninja <type1ninja@gmail.com>
2017-07-30 19:12:47 -0500

    Grenades Destroy Themselves
    Grenades now destroy themselves when any of these three conditions are
    met: A) The grenade has been active for 20 seconds, B) 0.25 seconds have
    passed since the grenade passed within 6 meters of a player, or C) The
    grenade hit something directly. The idea behind those parameters is to
    allow players to, respectively:  A) Put land mines (or space mines?) in
    orbit, B) Hit players without having godlike accuracy, and C) Be
    rewarded for actually having godlike accuracy.

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+
+    private float despawnLifetime = 20.0f;
+    private float fuseTime = 0.25f;
+    bool triggered = false;
+
+	private void Update () {
+		if (triggered)
+        {
+            fuseTime -= Time.deltaTime;
+        }
+
+        despawnLifetime -= Time.deltaTime;
+
+        if (fuseTime <= 0 || despawnLifetime <= 0)
+        {
+            Explode();
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            triggered = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Explode();
+    } 
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+    }
+}
