e2aa7f0c71d7e66bf2f58d17e3cb80aba2e1ce7f
Type1Ninja <type1ninja@gmail.com>
2017-07-31 16:28:15 -0500

    Grenades are Networked
    Grenades are now networked. They still don't have any particle effects
    and they don't deal any damage. Particle effects are probably coming
    first because they make damage easier.

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -2,6 +2,10 @@
 
 public class Grenade : MonoBehaviour {
 
+    //Keeps track of whether the local player spawned this or another player. Basically, we only deal damage if we're local; otherwise
+    //the grenade is cosmetic.
+    bool isLocal = false;
+
     private float despawnLifetime = 20.0f;
     private float fuseTime = 0.25f;
     bool triggered = false;
@@ -30,11 +34,18 @@ public class Grenade : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
+        //TODO--Deal bonus damage if it's a player
         Explode();
     } 
 
     private void Explode()
     {
+        //Deal damage to every player in the trigger radius
         Destroy(gameObject);
+    }
+
+    public void SetIsLocal(bool newIsLocal)
+    {
+        isLocal = newIsLocal;
     }
 }
--- Assets/Scripts/Player/PlayerWeapons.cs
+++ Assets/Scripts/Player/PlayerWeapons.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 
-public class PlayerWeapons : MonoBehaviour {
+public class PlayerWeapons : Photon.MonoBehaviour {
 
     public GameObject GrenadePrefab;
 
     private Transform GrenadeSpawnPositionTransform;
     private Rigidbody rigbod;
     private Collider col;
+
+    private GameManager gameManager;
 
     private float grenadeForce = 10.0f;
     private float maxGrenadeCooldown = 0.5f;
@@ -19,26 +21,41 @@ public class PlayerWeapons : MonoBehaviour {
         rigbod = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
 
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
         currentGrenadeCooldown = maxGrenadeCooldown;
     }
 
     private void Update()
     {
-        if (Input.GetButton("PrimaryFire") && currentGrenadeCooldown <= 0)
+        if (photonView.isMine == false && PhotonNetwork.connected == true)
         {
-            FireGrenade(); 
+            return;
+        }
 
-            currentGrenadeCooldown = maxGrenadeCooldown;
+        if (!gameManager.GetIsPaused())
+        {
+            if (Input.GetButton("PrimaryFire") && currentGrenadeCooldown <= 0)
+            {
+                //Call the method for ourselves, then call it as an RPC for everyone else
+                FireGrenade(GrenadeSpawnPositionTransform.position, transform.rotation, true, rigbod.velocity, transform.forward * grenadeForce);
+                photonView.RPC("FireGrenade", PhotonTargets.Others, GrenadeSpawnPositionTransform.position,
+                    transform.rotation, false, rigbod.velocity, transform.forward * grenadeForce);
+
+                currentGrenadeCooldown = maxGrenadeCooldown;
+            }
         }
 
         currentGrenadeCooldown -= Time.deltaTime;
     }
 
-    private void FireGrenade()
+    [PunRPC]
+    private void FireGrenade(Vector3 pos, Quaternion rot, bool isLocal, Vector3 inheritedVelocity, Vector3 launchForce) 
     {
-        GameObject grenade = Instantiate(GrenadePrefab, GrenadeSpawnPositionTransform.position, Quaternion.identity);
-        grenade.GetComponent<Rigidbody>().AddForce(rigbod.velocity, ForceMode.VelocityChange);
-        grenade.GetComponent<Rigidbody>().AddForce(transform.forward * grenadeForce, ForceMode.Impulse); 
+        GameObject grenade = Instantiate(GrenadePrefab, pos, rot);
+        grenade.GetComponent<Grenade>().SetIsLocal(isLocal);
+        grenade.GetComponent<Rigidbody>().AddForce(inheritedVelocity, ForceMode.VelocityChange);
+        grenade.GetComponent<Rigidbody>().AddForce(launchForce, ForceMode.Impulse); 
 
         foreach (Collider grenadeCol in grenade.GetComponentsInChildren<Collider>())
         {
